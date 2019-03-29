58a59689cd855c91e6db1a6366ea761feaa5864b
nidhi12poddar <49054902+nidhi12poddar@users.noreply.github.com>
2019-03-29 13:22:49 +0530

    sample pattern  generation program

---
+++ patt2.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 20 11:48:53 2019
+
+@author: NPL
+"""
+
+
+#3
+#4 4
+#5 5 5
+#6 6 6 6
+#7 7 7 7 7
+
+
+
+#3 3
+4 4
+4 4
+5 5
+5 5
+5 5
+6 6
+6 6
+6 6
+6 6
+7 7
+7 7
+7 7
+7 7
+7 7####
+def pattern1():
+  i= 0
+  j=0
+  s=3
+  while(i<5):
+            j = 0
+            while (j <=i):
+                                     print s                              
+                                     j = j + 1
+            s = s + 1
+            i = i + 1
+            #print(text, end ='')
+pattern1()
+
+###############################
+
+#output 
+#h a c k e r 
+
+def pattern1():
+  i= 0
+  j=0
+  s=3   
+  while(i<5) :  
+              j = 0
+              while (j <=i):
+                            
+                             print(str(s))
+                             j = j + 1
+              s = s + 1
+              i = i + 1  
+  
+             #print(text, end='')
+pattern1()   
+#########################################
